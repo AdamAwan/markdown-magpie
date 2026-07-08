a5d4ad4d36fada8859935f1710a16a69123e1fab
Adam Awan <adam.awan121@gmail.com>
2026-07-08 01:17:32 +0100

    feat(source-map): add source map view to web console
    - Add SourceMapEntry to type re-exports and new SourceMapResponse type
    - Add 'source-map' to ConsoleSection and nav entry with 'Sm' glyph
    - Fetch source map entries in ConsoleProvider when config has sources
    - New SourceMapPanel: grouped by source, shows topics/paths/descriptions/timestamps
    - New /source-map page routing

---
+++ apps/web/src/app/source-map/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useConsole } from "../../components/ConsoleProvider";
+import { SourceMapPanel } from "../../components/SourceMapPanel";
+import { Workbench } from "../../components/ui";
+
+export default function SourceMapPage() {
+  const { config, sourceMapEntries } = useConsole();
+
+  return (
+    <Workbench>
+      <SourceMapPanel
+        entries={sourceMapEntries}
+        sources={config?.knowledge?.sources}
+      />
+    </Workbench>
+  );
+}
